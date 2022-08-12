925fdbc8a5020353a8776ab3f30d1352b7adcd03
zhaoyimin <18510512011@163.com>
2022-08-13 03:59:41 +0800

    feat(custom): feat(custom): add ast parse element node

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -20,12 +20,15 @@ describe('Parse', () => {
 
   describe('element', () => {
     it('simple element div', () => {
-      const ast = baseParse('<div></div>')
+      let ast = baseParse('<div></div>')
 
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.ELEMENT,
         tag: 'div'
       })
+
+      ast = baseParse('<1div></1div>')
+      expect(ast.children[0]).toBeUndefined()
     })
   })
 })
